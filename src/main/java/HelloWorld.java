b9e6123254b1febae11f7dcce13e85ca124fd8b7
cloudkitas@gmail.com <BrunoCohen@Brunos-MacBook-Pro-2.local>
2023-04-14 12:08:54 +0100

    Part 1 - Introduction Section

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,25 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("\tI love pizza");
+        //escape sequences are denoted by backslash \
+        //used to send special characters , like new line (enter key)
+
+        //tab example
+        System.out.println("\t This escape sequence has a tab");
+
+        //quotes
+        System.out.println("\" This text is within quotes \" ");
+
+
+        // a backslash symbol within the print text
+        System.out.println("The following is a backslash \\");
+
+        /*
+
+        This is a comment block
+
+         */
+
         System.out.println("Its really good ");
 
         System.out.println("This is a code from Bk Branch");
