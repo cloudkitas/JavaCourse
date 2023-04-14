7e37dd744e72ef103d0951c2661e58c0acb407fc
cloudkitas@gmail.com <BrunoCohen@Brunos-MacBook-Pro-2.local>
2023-04-14 09:33:41 +0100

    Added a new println to branch bkdevoldmac - just testing git

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("\tI love pizza");
         System.out.println("Its really good ");
+
+        System.out.println("This is a code from Bk Branch");
     }
 }
