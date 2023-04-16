e33c5734262a93e102287feca8415f219a2b689c
cloudkitas@gmail.com <BrunoCohen@Brunos-MacBook-Pro-2.local>
2023-04-16 11:57:44 +0100

    Part 2 - Variables Section

---
+++ src/main/java/Variables.java
@@ -0,0 +1,36 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        // 8 primitive data types in java
+        /*
+        Boolean
+        byte
+        short
+        int
+        long
+
+        float
+        double
+
+        char
+        String (Reference Data type)
+
+         */
+
+        int x; //declaration
+
+        x = 123; //assignment
+
+        int y = 123; // initialization
+
+        System.out.println(x + y);
+
+        float z = 3.14f;
+        System.out.println(z);
+
+        boolean t = true;
+        char symbol = '@';
+        System.out.println(symbol);
+    }
+
+}
